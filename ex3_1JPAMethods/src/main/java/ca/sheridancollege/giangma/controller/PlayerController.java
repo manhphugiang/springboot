666c22231160db5bd3417a0a2294548db779e2a5
Manh Phu Giang <gmanhphuu@gmail.com>
2025-01-20 23:00:10 -0500

    changes ex3-1

--- ex3_1JPAMethods/src/main/java/ca/sheridancollege/giangma/controller/PlayerController.java
+++ ex3_1JPAMethods/src/main/java/ca/sheridancollege/giangma/controller/PlayerController.java
@@ -1,14 +1,20 @@
 package ca.sheridancollege.giangma.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ca.sheridancollege.giangma.beans.Player;
+import ca.sheridancollege.giangma.beans.Videogame;
 import ca.sheridancollege.giangma.repository.PlayerRepository;
 import lombok.AllArgsConstructor;
 
@@ -39,9 +45,56 @@ public class PlayerController {
 	
 	
 	@GetMapping("/view")
-	public String viewPage(Model model) {
-		model.addAttribute("player", pRepo.findAll());
-		return "viewPlayer.html";
+	public String viewPage(@RequestParam(name = "sortBy", required = false, defaultValue = "id") String sortBy, Model model) {
+	    List<Player> sortedPlayers;
+
+	    switch (sortBy) {
+	        case "name":
+	            sortedPlayers = pRepo.findByOrderByNameAsc();
+	            break;
+	        case "guildPoint":
+	            sortedPlayers = pRepo.findByOrderByGuildPointAsc();
+	            break;
+	        case "unitClass":
+	            sortedPlayers = pRepo.findByOrderByUnitClassAsc();
+	            break;
+	        default:
+	            sortedPlayers = pRepo.findAll();
+	    }
+
+	    model.addAttribute("player", sortedPlayers);
+	    return "viewPlayer.html";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String goEdit(@PathVariable int id, Model model) {
+		Optional<Player> player = pRepo.findById(id);
+		if(player.isPresent()) { // there is a game with this id
+			Player p = player.get();
+			model.addAttribute("player", p);
+			return "editPlayer.html";
+		}else { // no game with that id
+			return "redirect:/view";
+		}
+	}
+	
+	@PostMapping("/edit")
+	public String processingEditGame(@ModelAttribute Player player) {
+		pRepo.save(player);
+		return "redirect:/view";
+	}
+	
+	
+	@GetMapping("/delete/{id}")
+	public String processingDeleteGame(@PathVariable int id, Model model) {
+		Optional<Player> player = pRepo.findById(id);
+		if(player.isPresent()) { // there is a game with this id
+			Player p = player.get();
+			pRepo.delete(p);
+			return "redirect:/view";
+		}else { // no game with that id
+			return "redirect:/view";
+		}		
 	}
 	
 
--- ex3_1JPAMethods/src/main/java/ca/sheridancollege/giangma/repository/PlayerRepository.java
+++ ex3_1JPAMethods/src/main/java/ca/sheridancollege/giangma/repository/PlayerRepository.java
@@ -1,5 +1,7 @@
 package ca.sheridancollege.giangma.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,10 @@ import ca.sheridancollege.giangma.beans.Player;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
-	
+	public List<Player> findByOrderByIdAsc();
+	public List<Player> findByOrderByNameAsc();
+    public List<Player> findByOrderByGuildPointAsc();
+    public List<Player> findByOrderByUnitClassAsc();
 }
 
 
