aabb1201545f657d71104410fa14ad58a681d2b1
Manh Phu Giang <gmanhphuu@gmail.com>
2025-02-22 23:12:31 -0500

    test

---
+++ src/main/java/ca/sheirdancollege/giangma/testonmac/TestOnMacApplication.java
@@ -0,0 +1,13 @@
+package ca.sheirdancollege.giangma.testonmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOnMacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOnMacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheirdancollege/giangma/testonmac/TestOnMacApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheirdancollege.giangma.testonmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOnMacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
